5a71d715ce319984662ea32bbd41f53718f1f09d
rae1 <rael.neninger@gmail.com>
2014-06-13 11:09:04 -0400

    Added problems namespace; added problem 1 implementation file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import problems.one
+
 def main():
     print('Hello, World!')
+    problems.one.find_sum_of_multiples(10)
 
 if __name__ == '__main__':
     main()
---
+++ problems/one.py
@@ -0,0 +1,3 @@
+def find_sum_of_multiples(number):
+    print('Finding the sum of a bunch of multiples!')
+    return
