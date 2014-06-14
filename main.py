3ffbd97bfdc2f625384a31922c2c3c01441c0deb
rae1 <rael.neninger@gmail.com>
2014-06-14 08:41:39 -0400

    Finished implementation of problem one

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import problems.one
 
 def main():
     print('Hello, World!')
-    problems.one.find_sum_of_multiples(10)
+    problems.one.find_sum_of_multiples(1000, [3, 5])
 
 if __name__ == '__main__':
     main()
--- problems/one.py
+++ problems/one.py
@@ -1,3 +1,22 @@
-def find_sum_of_multiples(number):
+def find_sum_of_multiples(limit, numbers):
+    """
+    Multiples of 3 and 5
+    Problem 1
+    If we list all the natural numbers below 10 that are multiples of 3 or 5,
+    we get 3, 5, 6 and 9. The sum of these multiples is 23.
+    Find the sum of all the multiples of 3 or 5 below 1000.
+    """
     print('Finding the sum of a bunch of multiples!')
-    return
+
+    sumofmultiples = sum(find_all_divisible_in_range(limit, numbers))
+    print()
+    print(sumofmultiples)
+    return sumofmultiples
+
+
+def find_all_divisible_in_range(limit, numbers):
+    for current in range(1, limit):
+        divisible = map(lambda number: current % number == 0, numbers)
+        if any(divisible):
+            print(current, end=' ')
+            yield current
