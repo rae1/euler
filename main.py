17357a474005e9778d0327238c3b6ab424ea49a6
rae1 <rael.neninger@gmail.com>
2014-06-14 17:41:49 -0400

    Fixed bug due to project restructure

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import problems.one
+import one
 
 def main():
     print('Hello, World!')
-    problems.one.find_sum_of_multiples(1000, [3, 5])
+    one.find_sum_of_multiples(1000, [3, 5])
 
 if __name__ == '__main__':
     main()
