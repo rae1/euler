5b0e7f230c1322849cc8daaa4141766922c74bec
rae1 <rael.neninger@gmail.com>
2014-06-15 15:58:55 -0400

    Changed problem one implementation to use a class

--- one.py
+++ one.py
@@ -1,4 +1,5 @@
-def find_sum_of_multiples(limit, numbers):
+
+class One():
     """
     Multiples of 3 and 5
     Problem 1
@@ -6,17 +7,22 @@ def find_sum_of_multiples(limit, numbers):
     we get 3, 5, 6 and 9. The sum of these multiples is 23.
     Find the sum of all the multiples of 3 or 5 below 1000.
     """
-    print('Finding the sum of a bunch of multiples!')
+    def __init__(self):
+        pass
 
-    sumofmultiples = sum(find_all_divisible_in_range(limit, numbers))
-    print()
-    print(sumofmultiples)
-    return sumofmultiples
+    def solve(self):
+        return self.find_sum_of_multiples(1000, [3, 5])
 
+    def find_sum_of_multiples(self, limit, numbers):
 
-def find_all_divisible_in_range(limit, numbers):
-    for current in range(1, limit):
-        divisible = map(lambda number: current % number == 0, numbers)
-        if any(divisible):
-            print(current, end=' ')
-            yield current
+        print('Finding the sum of a bunch of multiples!')
+
+        total_sum = sum(self.__find_all_divisible_in_range(limit, numbers))
+        return total_sum
+
+    def __find_all_divisible_in_range(self, limit, numbers):
+        for current in range(1, limit):
+            divisible = map(lambda number: current % number == 0, numbers)
+            if any(divisible):
+                print(current, end=' ')
+                yield current
